33f7956b776eb4ae6c60641b75a419d84e7ea999
Alexis Jeandet <alexis.jeandet@member.fsf.org>
2022-04-09 19:59:59 +0200

    Whole stack update
    Signed-off-by: Alexis Jeandet <alexis.jeandet@member.fsf.org>

--- images/LPP/build_and_push.sh
+++ images/LPP/build_and_push.sh
@@ -1,5 +1,8 @@
-#!/usr/bin/sh
+#!/usr/bin/bash
+HUB_VERSION=${2:-"2.2.2"}
+REGISTRY=${1:-"129.104.6.165:32219"}
+echo "Will build images for dockerhub version: $HUB_VERSION and push on registry: $REGISTRY"
 
-cd gpu && sh build_and_push.sh; cd ..
-cd hpc && sh build_and_push.sh; cd ..
-cd space && sh build_and_push.sh; cd ..
+cd gpu && bash build_and_push.sh $REGISTRY $HUB_VERSION; cd ..
+cd hpc && bash build_and_push.sh  $REGISTRY $HUB_VERSION; cd ..
+cd space && bash build_and_push.sh $REGISTRY $HUB_VERSION; cd ..

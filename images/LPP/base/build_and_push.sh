5cbd4333e30aa938838a5d5ac73b8d7ec3e0ae99
Alexis Jeandet <alexis.jeandet@member.fsf.org>
2022-04-15 19:16:50 +0200

    Refactor Docker images to depend on a common one with JupyterLab plugins installed
    Signed-off-by: Alexis Jeandet <alexis.jeandet@member.fsf.org>

--- images/LPP/base/build_and_push.sh
+++ images/LPP/base/build_and_push.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 HUB_VERSION=${2:-"2.2.2"}
 REGISTRY=${1:-"129.104.6.165:32219"}
-IMAGE="lpp/jupyter-notebok-space"
+IMAGE="lpp/jupyter-notebok-base"
 echo "Will build $IMAGE for dockerhub version: $HUB_VERSION and push on registry: $REGISTRY"
 FULL_NAME="${REGISTRY}/${IMAGE}:hub-${HUB_VERSION}"
 docker build --build-arg HUB_VERSION=$HUB_VERSION -t ${FULL_NAME} .

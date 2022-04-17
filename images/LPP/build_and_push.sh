568ee566d4c204582b7c4df4cd1423d16da54fe0
Alexis Jeandet <alexis.jeandet@member.fsf.org>
2022-04-17 20:56:20 +0200

    Adds image with VSCode (code-server) \o/
    Signed-off-by: Alexis Jeandet <alexis.jeandet@member.fsf.org>

--- images/LPP/build_and_push.sh
+++ images/LPP/build_and_push.sh
@@ -7,3 +7,5 @@ cd base && bash build_and_push.sh $REGISTRY $HUB_VERSION; cd ..
 cd gpu && bash build_and_push.sh $REGISTRY $HUB_VERSION; cd ..
 cd hpc && bash build_and_push.sh  $REGISTRY $HUB_VERSION; cd ..
 cd space && bash build_and_push.sh $REGISTRY $HUB_VERSION; cd ..
+cd code_oss && bash build_and_push.sh $REGISTRY $HUB_VERSION; cd ..
+

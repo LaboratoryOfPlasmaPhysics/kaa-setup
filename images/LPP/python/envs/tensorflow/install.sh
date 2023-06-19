40138f983125c274308f9b870c54b872f64dc6de
Alexis Jeandet <alexis.jeandet@member.fsf.org>
2023-06-19 15:26:36 +0200

    Split tensorflow and pytorch envs
    Signed-off-by: Alexis Jeandet <alexis.jeandet@member.fsf.org>

--- images/LPP/python/envs/tensorflow/install.sh
+++ images/LPP/python/envs/tensorflow/install.sh
@@ -7,3 +7,5 @@ CONDA_OVERRIDE_CUDA="11.7.1" mamba env create -f $( dirname -- "$0"; )/environme
 
 mamba run -n tensorflow /opt/conda/envs/tensorflow/bin/python  $( dirname -- "$0"; )/tests/all.py
 cp $( dirname -- "$0"; )/logo-*.png /opt/conda/envs/tensorflow/share/jupyter/kernels/python3/
+echo 'CUDNN_PATH=/opt/conda/envs/tensorflow/lib/python3.10/site-packages/nvidia/cudnn/' >> /opt/conda/envs/tensorflow/etc/conda/activate.d/env_vars.sh
+echo 'export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/conda/envs/tensorflow/lib/:$CUDNN_PATH/lib' >> /opt/conda/envs/tensorflow/etc/conda/activate.d/env_vars.sh
